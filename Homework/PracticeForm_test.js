a8476a6d25a003061e7ca5abb0f84441dfe3786a
Łukasz Niestrój <lukas.niestroj@gmail.com>
2021-07-15 15:27:59 +0200

    update bday date field

--- Homework/PracticeForm_test.js
+++ Homework/PracticeForm_test.js
@@ -7,11 +7,15 @@ Scenario('PracticeForm_ComplementForm', ({ I }) => {
     I.fillField("//input[@id='userEmail']", "lukas.nnn@gmail.com");
     I.click('Male');
     I.fillField("//input[@id='userNumber']", "111111111");
-    I.fillField("//input[@id='dateOfBirthInput']", "12 May 1994");
-    I.fillField("//div[@class='subjects-auto-complete__value-container subjects-auto-complete__value-container--is-multi css-1hwfws3']", "QE Tester");
+    I.click("//input[@id='dateOfBirthInput']");
+    I.click("//select[@class='react-datepicker__year-select']");
+    I.selectOption("//select[@class='react-datepicker__year-select']", '1994');
+    I.selectOption("//select[@class='react-datepicker__month-select']", "May");
+    I.click("//div[@class='react-datepicker__day react-datepicker__day--012']");
+    I.fillField("//div[@class='subjects-auto-complete__value-container subjects-auto-complete__value-container--is-multi css-1hwfws3']", 'Physics');
     I.click('Sport');
     I.fillField("//textarea[@id='currentAddress']", "ul. Powstańców Śląskich");
-    I.fillField("//div[contains(text(),'Select State')]", "NCR");
+    I.click("//div[contains(text(),'Select State')]");
     I.selectOption("//div[contains(text(),'Select City')]", "Delhi");
     I.click("//button[@id='submit']");
 
