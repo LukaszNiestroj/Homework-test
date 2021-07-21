32e5d77725b75bb6f82721c1e2ae91e81a8c5278
Łukasz Niestrój <lukas.niestroj@gmail.com>
2021-07-21 13:36:32 +0200

    update subjects and state selection

--- Homework/PracticeForm_test.js
+++ Homework/PracticeForm_test.js
@@ -3,21 +3,39 @@ Feature('PracticeForm');
 Scenario('PracticeForm_ComplementForm', ({ I }) => {
     I.amOnPage('https://demoqa.com/automation-practice-form');
     I.fillField("//input[@id='firstName']", "Łukasz");
+    I.wait(1)
     I.fillField("//input[@id='lastName']", "Niestrój");
+    I.wait(1)
     I.fillField("//input[@id='userEmail']", "lukas.nnn@gmail.com");
+    I.wait(1)
     I.click('Male');
+    I.wait(1)
     I.fillField("//input[@id='userNumber']", "111111111");
+    I.wait(1)
     I.click("//input[@id='dateOfBirthInput']");
+    I.wait(1)
     I.click("//select[@class='react-datepicker__year-select']");
+    I.wait(1)
     I.selectOption("//select[@class='react-datepicker__year-select']", '1994');
+    I.wait(1)
     I.selectOption("//select[@class='react-datepicker__month-select']", "May");
+    I.wait(1)
     I.click("//div[@class='react-datepicker__day react-datepicker__day--012']");
-    I.fillField("//div[@class='subjects-auto-complete__value-container subjects-auto-complete__value-container--is-multi css-1hwfws3']", 'Physics');
+    I.wait(1)
+    I.fillField("//input[@id='subjectsInput']", "physics");
+    I.click("//div[contains(@class,'option') and contains(text(),'Physics')]");
+    I.wait(1)
     I.click('Sport');
     I.fillField("//textarea[@id='currentAddress']", "ul. Powstańców Śląskich");
+    I.wait(1)
     I.click("//div[contains(text(),'Select State')]");
-    I.click("//div[@class='mt-4 justify-content-end row']", "NCR");
-    I.click("//div[contains(text(),'Select City')]", "Delhi");
+    I.wait(1)
+    I.click("//div[contains(@class,'option') and contains(text(),'NCR')]");
+    I.wait(1)
+    I.click("//div[contains(text(),'Select City')]");
+    I.wait(1)
+    I.click("//div[contains(@class,'option') and contains(text(),'Delhi')]");
+    I.wait(1)
     I.click("//button[@id='submit']");
 
 
